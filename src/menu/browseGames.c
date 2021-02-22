288ac0c6e0dc95a6e885a9b6780988d5d19bae21
x7l7j8cc <x7l7j8cc@gmail.com>
2021-02-22 22:47:33 +0100

    Update browseGames.c

--- src/menu/browseGames.c
+++ src/menu/browseGames.c
@@ -1,41 +1,43 @@
 // --- TEST FILE ---
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 //Function to Print the Game/DLC List on NUSspli
-void BrowseDList(char *gameTitle) {
-   char * token = strtok(gameTitle, "|");
-   int cntTK = 0; int cntLs = 0;
-   while( token != NULL ) {
-      
+void BrowseDList(char *gameTitle, int cLines) {
+   char * gTitle = strtok(gameTitle, "|");
+   int cntTK = 0;
+   while( gTitle != NULL ){
       switch(cntTK){
          //Title Name
-         case 0: printf("%u. %s ", cntLs, token); token=strtok(NULL,"|"); cntTK++; break;
+         case 0: printf("%u. %s ", cLines, gTitle); gTitle=strtok(NULL,"|"); cntTK++; break;
          //Title ID
-         case 1: printf("[%s]", token); token=strtok(NULL,"|"); cntTK++; break;
+         case 1: printf("[%s]", gTitle); gTitle=strtok(NULL,"|"); cntTK++; break;
          //Title Region
-         case 2: printf(" (%s)\n", token); token=strtok(NULL,"|"); cntTK=0; break;
+         case 2: printf(" (%s)\n", gTitle); gTitle=strtok(NULL,"|"); cntTK=0; break;
       }
-      cntLs=cntLs+1;
+      
    }
    return;
 }
 
-//Function to Read the Games/DLC Database
-int readDBGames() {
-  FILE *fp = fopen("DB_Games.txt", "r");
-  if(fp == NULL) { perror("Unable to open DB_Games file!"); exit(1); }
+//Function to Read the Games/DLC from Database
+int readDBGames(char *DBfile) {
+  FILE *fp = fopen(DBfile, "r");
+  if(fp == NULL) { fprintf(stderr, "!!--> Unable to open %s file <--!!", DBfile); exit(1); }
   char *line = NULL;
   size_t len = 0;
+  int count_lines = 0;
   while(getline(&line, &len, fp) != -1) {
+    count_lines++;
     line[strcspn(line, "\r\n")] = 0;
-    BrowseDList( line );
+    BrowseDList( line, count_lines );
   }
   fclose(fp); free(line);
 }
 
 int main(){
-   readDBGames(); //Init Read Database
+   readDBGames("DB_Games.txt"); //Init Read Database
    return 0;
 }
